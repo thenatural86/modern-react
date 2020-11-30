6af15912f5a9e910768341b3b32bd94c1a4d24f9
thenatural86 <philip.smalls86@gmail.com>
2020-11-29 19:04:32 -0500

    click handlers

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -1,41 +1,72 @@
 import React, { useState } from 'react'
 import people from './data'
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaPeopleCarry,
+  FaQuoteRight,
+} from 'react-icons/fa'
 
 const Review = () => {
   // console.log(people)
   const [index, setIndex] = useState(0)
-  const { id, name, job, image, text } = people[index]
+  const { name, job, image, text } = people[index]
+
+  const checkNum = (num) => {
+    if (num > people.length - 1) {
+      return 0
+    }
+    if (num < 0) {
+      return people.length - 1
+    }
+    return num
+  }
+
+  const prevClick = () => {
+    setIndex((index) => {
+      let newIndex = index - 1
+      return checkNum(newIndex)
+    })
+  }
+
+  const nextClick = () => {
+    setIndex((index) => {
+      let newIndex = index + 1
+      return checkNum(newIndex)
+    })
+  }
+
+  const yoloClick = (index) => {
+    index = Math.floor(Math.random() * people.length)
+    setIndex(index)
+  }
+
   return (
     <article className='review'>
-      <div key={id}>
-        <h2>review component</h2>
-        <div className='img-container'>
-          <img src={image} alt={name} className='person-img' />
-          <span className='quote-icon'>
-            <FaQuoteRight />
-          </span>
-        </div>
+      <h2>review component</h2>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
       </div>
+
       <h4 className='author'>{name}</h4>
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='prev-btn'>
+        <button onClick={prevClick} className='prev-btn'>
           <FaChevronLeft />
         </button>
-        <button className='next-btn'>
+        <button onClick={nextClick} className='next-btn'>
           <FaChevronRight />
         </button>
       </div>
-      <button className='random-btn'>Yolo Button</button>
+      <button onClick={yoloClick} className='random-btn'>
+        Yolo Button
+      </button>
     </article>
   )
-}
-{
-  // <FaChevronLeft />
-  {
-  }
 }
 
 export default Review
