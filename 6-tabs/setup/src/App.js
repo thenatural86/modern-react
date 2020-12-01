1ec043aad7fdd0dfb6a9ae52dcfc73f288d5d724
thenatural86 <philip.smalls86@gmail.com>
2020-11-30 19:48:18 -0500

    styles

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -4,33 +4,53 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading] = useState(true)
   const [tabs, setTabs] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [value, setValue] = useState(0)
 
   const getData = async () => {
     const resp = await fetch(url)
     const data = await resp.json()
     setTabs(data)
+    setLoading(false)
   }
   console.log(tabs)
   useEffect(() => {
     getData()
   }, [])
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+  const { company, dates, duties, id, order, title } = tabs[value]
   return (
-    <main>
-      {tabs.map((tab) => {
-        const { company, dates, duties, id, order, title } = tab
-        return (
-          <section key={id}>
-            <h4>{title}</h4>
-            <h4>{company}</h4>
-            <h4>{dates}</h4>
-            <h4>{duties}</h4>
-            <h4>{order}</h4>
-          </section>
-        )
-      })}
-    </main>
+    <section className='section' key={id}>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* button container */}
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
   )
 }
 
