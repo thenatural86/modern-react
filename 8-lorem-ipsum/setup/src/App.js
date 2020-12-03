5c4413da8b0b2225260f7e2444e06a36271dfb83
thenatural86 <philip.smalls86@gmail.com>
2020-12-03 12:30:35 -0500

    form basics

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
+import data from './data'
 function App() {
-  
-  
+  const [texts, setTexts] = useState([])
+  const [count, setCount] = useState(0)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('yolo world')
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>Tired of the same ol Lorem Ipsum?</h3>
+      <form className='lorem-form' onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input type='number' name='amount' id='amount' />
+      </form>
+      <article>
+        {texts.map((text) => {
+          return (
+            <div>
+              <p>{text}</p>
+            </div>
+          )
+        })}
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
