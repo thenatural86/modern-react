2317e90cb99c67af96897da9eeff28f09e5f06a9
thenatural86 <philip.smalls86@gmail.com>
2020-12-03 12:38:04 -0500

    project structure

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -12,18 +12,35 @@ function App() {
   return (
     <section className='section-center'>
       <h3>Tired of the same ol Lorem Ipsum?</h3>
-      <form className='lorem-form' onSubmit={(e) => handleSubmit(e)}>
+      <form className='lorem-form' onSubmit={handleSubmit}>
         <label htmlFor='amount'>paragraphs:</label>
-        <input type='number' name='amount' id='amount' />
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
       </form>
-      <article>
-        {texts.map((text) => {
+      <article className='lorem-text'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+          aperiam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+          aperiam.
+        </p>
+        {/* {texts.map((text) => {
           return (
             <div>
               <p>{text}</p>
             </div>
           )
-        })}
+        })} */}
       </article>
     </section>
   )
