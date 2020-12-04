2cbdc4fee4e58dd1f666e300bda24e9cccac676a
thenatural86 <philip.smalls86@gmail.com>
2020-12-04 15:27:33 -0500

    finish colors

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#93ede9').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -27,7 +27,7 @@ function App() {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#f15025'
+            placeholder='#93ede9'
             className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='submit'>
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
-  // const hex = rgbToHex(...rgb)
-  const hex = hexColor
-  console.log(hexColor)
   const hexValue = `#${hexColor}`
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [alert])
   return (
     <article
       className={`color ${index > 10 ? 'color-light' : null}`}
