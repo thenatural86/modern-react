d397369c7c6af2f711e6ad8c5c61331cfb492fcf
thenatural86 <philip.smalls86@gmail.com>
2020-12-05 14:39:37 -0500

    init state values

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,33 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({})
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(name)
+  }
+  return (
+    <section className='container'>
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder='grocery item'
+        />
+        <button className='btn' type='submit'>
+          Submit
+        </button>
+      </form>
+      <List />
+      <Alert />
+    </section>
+  )
 }
 
 export default App
