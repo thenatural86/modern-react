774cabe95a05762cf38c30b03c05f5798fe94ee2
thenatural86 <philip.smalls86@gmail.com>
2020-12-09 17:16:58 -0500

    final touches

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -14,7 +14,11 @@ const Navbar = () => {
     openSubmenu(page, { center, bottom })
   }
 
-  const handleSubmenu = () => {}
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu()
+    }
+  }
 
   return (
     <nav className='nav' onMouseOver={handleSubmenu}>
