ce03f984af28890a91e2bf25d6894ffa91fc4cf3
thenatural86 <philip.smalls86@gmail.com>
2020-12-09 12:38:05 -0500

    hero component

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -3,9 +3,26 @@ import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
-  const data = useGlobalContext()
-  console.log(data)
-  return <h2>hero component</h2>
+  const { closeSubmenu } = useGlobalContext()
+
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit
+            quisquam maiores odio asperiores ratione distinctio hic illum! Nisi
+            dolore ipsam rerum sit quae.
+          </p>
+          <button className='btn'>Start Now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
