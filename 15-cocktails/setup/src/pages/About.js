2db6f30273726b5911f6718e6cb926d71ebd7165
thenatural86 <philip.smalls86@gmail.com>
2020-12-16 20:36:43 -0500

    error  and about pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo sint
+        ex iure, tempore, minima veniam praesentium laborum accusamus aliquam
+        necessitatibus nihil pariatur et odio! Placeat iusto adipisci
+        repellendus beatae dolore aut, fugit in quia tempora eveniet, cum
+        laudantium itaque id.
+      </p>
+    </section>
   )
 }
 
