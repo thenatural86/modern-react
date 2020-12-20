a30a0c3b5182f95e743a98731ae242112d32836a
thenatural86 <philip.smalls86@gmail.com>
2020-12-20 11:35:23 -0500

    notes

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -5,13 +5,19 @@ const paginate = (followers) => {
   const pages = Math.ceil(followers.length / itemsPerPage)
   // console.log(pages)
 
-  // create an new array and give it a length property that is equal to what I pass in. in this case length is equal to the number of pages.
+  // create an new array and pass in a object with length property that is equal to what I pass in, in this case length is equal to the number of pages.
+  // second argument is a callback function that iterates over our new array and uses items from old array to create our new arrays.
+  // with 'item' passed in as a '_' for undefined and a index.
+  // As we iterate over what we return will be placed in that index.
   const newFollowers = Array.from({ length: pages }, (_, index) => {
+    // start will become starting index for each sub-array
     const start = index * itemsPerPage
     // console.log(start)
+    // then slice the how list (followers), from start, which changes on each iteration and ends with start + itemsPerPage
     return followers.slice(start, start + itemsPerPage)
   })
   console.log(newFollowers)
+  // newFollowers is the array of arrays
   return newFollowers
 }
 
