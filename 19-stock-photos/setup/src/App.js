0abd93ebca812724b241fda58c3e97e124ec1bb9
thenatural86 <philip.smalls86@gmail.com>
2020-12-21 14:21:28 -0500

    search

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -3,30 +3,43 @@ import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
+  const [query, setQuery] = useState('')
 
   const fetchImages = async () => {
     setLoading(true)
     let url
-    let urlPage = `&page=${page}`
-    url = `${mainUrl}${clientID}${urlPage}`
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+
     try {
       const response = await fetch(url)
       const data = await response.json()
       setPhotos((oldPhotos) => {
-        return [...oldPhotos, ...data]
+        if (query && page === 1) {
+          return data.results
+        } else if (query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
       })
       setLoading(false)
     } catch (error) {
-      setLoading(false)
       console.log(error)
+      setLoading(false)
     }
   }
   // fetch images on loading
@@ -56,14 +69,20 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    setPage(1)
   }
 
   return (
     <main>
       <section className='search'>
         <form className='search-form'>
-          <input type='text' placeholder='search' className='form-input' />
+          <input
+            type='text'
+            placeholder='search'
+            className='form-input'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button className='submit-btn' type='submit' onClick={handleSubmit}>
             <FaSearch />
           </button>
@@ -71,8 +90,9 @@ function App() {
       </section>
       <section className='photos'>
         <div className='photos-center'>
-          {photos.map((item) => {
-            return <Photo key={item.id} {...item} />
+          {photos.map((item, index) => {
+            console.log(item)
+            return <Photo key={index} {...item} />
           })}
         </div>
         {loading ? <h2 className='loading'>Loading...</h2> : null}
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Photo = ({
-  id,
   urls: { regular },
   alt_description,
   likes,
