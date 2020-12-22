cf7ee38ee2593451af189b69e69ae8d586568da8
thenatural86 <philip.smalls86@gmail.com>
2020-12-22 09:57:41 -0500

    init dark mode

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -3,7 +3,22 @@ import data from './data'
 import Article from './Article'
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>over-reacted</h1>
+          <button className='btn'>tiggle/toggle</button>
+        </div>
+      </nav>
+      <section className='articles'>
+        {data.map((item) => {
+          console.log(item)
+          return <Article key={item.id} {...item} />
+        })}
+      </section>
+    </main>
+  )
 }
 
 export default App
