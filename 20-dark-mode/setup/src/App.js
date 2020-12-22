c4327f08c660a0eb0803af466e21b6473468b153
thenatural86 <philip.smalls86@gmail.com>
2020-12-22 11:00:07 -0500

    local storage for user settings

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+// local storage to retain user preferences
+const getStorageTheme = () => {
+  let theme = 'light-theme'
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
 function App() {
-  const [theme, setTheme] = useState('light-theme')
+  const [theme, setTheme] = useState(getStorageTheme())
+  localStorage.setItem('theme', theme)
 
   const toggleTheme = () => {
     if (theme === 'light-theme') {
@@ -15,7 +25,6 @@ function App() {
 
   useEffect(() => {
     document.documentElement.className = theme
-    // setTheme('dark-theme')
   }, [theme])
 
   return (
