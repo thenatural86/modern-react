bf41a83322a3a81bedf2a8cde53a302ea83597d3
thenatural86 <philip.smalls86@gmail.com>
2020-12-25 11:34:56 -0500

    useReducer

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -16,6 +16,17 @@ const initialState = {}
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  // two values from useReducer, state and dispatch function.
+  // pass in reducer function, responsible for setting up state, and initial state
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // data fetching function
+  const fetchStories = async (url) => {
+    // to make a change to state use the dispatch function, which takes in an object
+    dispatch({ type: 'SET_LOADING' })
+    const response = await fetch(url)
+    const data = await response.json()
+  }
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
 // make sure use
