71b1aec2bd6b30dcdd1d34bdc02c2ab4f18a7bc3
thenatural86 <philip.smalls86@gmail.com>
2020-12-26 19:17:55 -0500

    state values in context

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -5,7 +5,11 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  return <h2>quiz starter</h2>
+  const { waiting, loading, questions, index, correct } = useGlobalContext()
+
+  if (waiting) return <SetupForm />
+  if (loading) return <Loading />
+  return <main>quiz app</main>
 }
 
 export default App
