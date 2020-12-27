a02aaa066cbf225d0782887b61ed4d54f1b08d93
thenatural86 <philip.smalls86@gmail.com>
2020-12-27 17:25:13 -0500

    data fetching

--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -24,6 +24,32 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
 
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(error))
+    // console.log(response)
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setLoading(false)
+        setQuestions(data)
+        setWaiting(false)
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(true)
+      }
+    } else {
+      setWaiting(true)
+    }
+  }
+  // console.log(questions)
+
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
+
   return (
     <AppContext.Provider
       value={{
